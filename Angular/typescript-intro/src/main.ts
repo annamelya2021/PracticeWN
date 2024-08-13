492dad7ccaec2a0e225e9270f020768db0554b85
annamelya2021 <anna.basyuk@gmail.com>
2024-08-13 14:38:54 +0200

    ts

---
+++ Angular/typescript-intro/src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+// import "./topics/01-basic-types"
+import "./topics/02-object-interface"
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `HOLA TS`
+console.log("Hola mundo")
---
+++ Angular/typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,16 @@
+const name : string = "Strider";
+let hpPoints : number | "FULL"; 
+const isAlive: boolean = true;
+
+
+
+hpPoints="FULL"
+console.log({
+    name,
+    hpPoints,
+    isAlive
+})
+
+
+
+export {}
