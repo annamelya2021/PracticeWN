304f663b1c20709c5496dedc61364abcc5b5e6d2
Anna Melnychenko <88847909+annamelya2021@users.noreply.github.com>
2024-08-26 12:44:51 +0200

    lazy menos svg

---
+++ Angular/03-gifs-app/src/app/shared/componets/lazyImage/lazyImage.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'shared-lazy-image',
+    templateUrl: './lazyImage.component.html',
+
+})
+export class LazyImageComponent implements OnInit{
+  @Input()
+    public url!: string;
+
+    @Input()
+    public alt: string ="";
+
+    public hasLoaded: boolean = false
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('Url is required');
+  }
+
+onLoad(){
+  setTimeout(() => {
+
+    this.hasLoaded = true;
+  }, 1000);
+
+  }
+
+ }
--- Angular/03-gifs-app/src/app/shared/shared.module.ts
+++ Angular/03-gifs-app/src/app/shared/shared.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './componets/sidebar/sidebar.component';
+import { LazyImageComponent } from './componets/lazyImage/lazyImage.component';
+
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    LazyImageComponent,
   ],
   imports: [
     CommonModule
   ],
   exports:[
     SidebarComponent,
+    LazyImageComponent
+
   ]
 })
 export class SharedModule { }
