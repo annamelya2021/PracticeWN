21357018392537b98c410ffb9255c2332997c531
Anna Melnychenko <88847909+annamelya2021@users.noreply.github.com>
2024-08-26 08:27:26 +0200

    section 8

--- Angular/03-gifs-app/src/app/app.module.ts
+++ Angular/03-gifs-app/src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WeatherComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    GifsModule,
+    SharedModule,
     HttpClientModule
   ],
   providers: [],
---
+++ exercicios/WeatherApp/temp-project/src/app/weather.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http:HttpClient) { }
+
+  getWeather(){
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=bilbao&appid=4079abfeea565fa5f716eec8df5dd8c7&units=imperial')
+  }
+}
