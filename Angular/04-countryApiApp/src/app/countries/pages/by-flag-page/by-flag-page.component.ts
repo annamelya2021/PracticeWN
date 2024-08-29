aeebc4e756a03199f656c4512443f4d44c98d49e
Anna Melnychenko <88847909+annamelya2021@users.noreply.github.com>
2024-08-29 13:02:22 +0200

    flag

---
+++ Angular/04-countryApiApp/src/app/countries/pages/by-flag-page/by-flag-page.component.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-by-flag-page',
+    templateUrl: './by-flag-page.component.html',
+    styleUrl: './by-flag-page.component.css',
+})
+export class ByFlagPageComponent { }
