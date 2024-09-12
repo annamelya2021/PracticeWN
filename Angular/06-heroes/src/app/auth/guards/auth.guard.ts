1f3f355ccbb540f12e6f2452841a0c8cc59ee3e4
Anna Melnychenko <88847909+annamelya2021@users.noreply.github.com>
2024-09-12 22:57:14 +0200

    update

---
+++ Angular/06-heroes/src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,38 @@
+import { Injectable, Pipe } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard  implements CanMatch, CanActivate{
+
+  constructor( private authService: AuthService,
+             private router: Router
+  ) { }
+
+  private checkAuthStatus(): boolean | Observable<boolean> {
+
+    return this.authService.checkauthentification()
+      .pipe(
+
+        tap(isAuthenticated => {
+          if (!isAuthenticated) {
+            this.router.navigate(['./auth/login'])
+          }
+        }),
+
+      )
+  }
+
+  canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> {
+    return this.checkAuthStatus()
+
+  }
+
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    return this.checkAuthStatus()
+  }
+
+}
